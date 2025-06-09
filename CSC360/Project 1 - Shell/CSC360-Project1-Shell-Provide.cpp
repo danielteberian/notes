565945738fa17e13c2d2964651625bfc3f3cd5f5
Daniel <72998334+danielteberian@users.noreply.github.com>
2025-06-08 22:11:34 -0500

    Update CSC360-Project1-Shell-Provide.cpp

--- CSC360/Project 1 - Shell/CSC360-Project1-Shell-Provide.cpp
+++ CSC360/Project 1 - Shell/CSC360-Project1-Shell-Provide.cpp
@@ -7,8 +7,6 @@
 #include <stdlib.h>
 #include <wait.h>
 #include <unistd.h>
-
-// Additional imports
 #include <sys/wait.h>
 #include <cstdlib>
 #include <cstring>
@@ -18,352 +16,271 @@
 
 using namespace std;
 
-
-// A counter for SIGINTs that are caught, set to 0 by default
 volatile int sigint_counter = 0;
-
-// Establish the history list
 vector<string> history;
-// The maximum length of the history list
 const int HIST_MAX = 10;
-
-// The number for the command (it is 1, since it is the most recent)
 int cmd_num = 1;
 
-// A handler for SIGINTs
-void sig_handler(int signal)
-{
-	// If the signal is SIGINT
-	if (signal == SIGINT)
-	{
-		// Increment the value of sigint_counter by 1
-		sigint_counter++;
-	}
+void sig_handler(int signal) {
+    if (signal == SIGINT) {
+        sigint_counter++;
+    }
 }
 
-
-
-void parse_args(string line, vector<string> &cmds){
+void parse_args(string line, vector<string> &cmds) {
     stringstream liness(line);
- 
     string token;
     while (getline(liness, token, ' ')) {
         cmds.push_back(token);
     }
 }
 
-
-// Parses input into a vector of string tokens
-void parse_cin_to_vec(const string& ln, vector<string>& tkns)
-{
-	// Clear any existing tokens
-	tkns.clear();
-
-	// Create a stringstream!
-	stringstream strstream(ln);
-	string tkn;
-
-	// Extract tokens, separated by whitespace
-	while (strstream >> tkn)
-	{
-		tkns.push_back(tkn);
-	}
+void parse_cin_to_vec(const string& ln, vector<string>& tkns) {
+    tkns.clear();
+    stringstream strstream(ln);
+    string tkn;
+    while (strstream >> tkn) {
+        tkns.push_back(tkn);
+    }
 }
 
+char** conv_vec_to_chararray(const vector<string>& tkns) {
+    if (tkns.empty()) {
+        return NULL;
+    }
 
-// Converts vector of C++ strings into an array of char pointers. This is done in order to meet the requirements for using execvp().
-char** conv_vec_to_chararray(const vector<string>& tkns)
-{
-	// If there are no tokens
-	if (tkns.empty())
-	{
-		// Don't do anything.
-		return NULL;
-	}
+    char** argv = static_cast<char**>(malloc((tkns.size() + 1) * sizeof(char*)));
 
-	// Allocate the appropriate amount of memory, given the array of char pointers.
-	// NOTE: The size is increased by one in order to make room for the NULL terminator.
-	char** argv = static_cast<char**>(malloc((tkns.size() + 1) * sizeof(char*)));
+    if (argv == NULL) {
+        perror("\033[1;31m[ERR] MALLOC FAILED FOR ARGV\033[0m");
+        return NULL;
+    }
 
-	if (argv == NULL)
-	{
-		perror("[ERR] MALLOC FAILED FOR ARGV");
-		// Return NULL
-		return NULL;
-	}
-
-	// Copy each string into a C-string
-	for (size_t i = 0; i < tkns.size(); ++i)
-	{
-		// Use strdup in order to allocate memory and copy string
-		argv[i] = strdup(tkns[i].c_str());
-
-		// If argv has no value
-		if (argv[i] == NULL)
-		{
-			perror("[ERR] STRDUP FAILED FOR ARGV");
-
-			// Clean up any allocated memory
-			for (size_t j = 0; j < i; ++j)
-			{
-				free(argv[j]);
-			}
-
-			free(argv);
-			return NULL;
-		}
-	}
-
-	// The array gets the ol' null terminator treatment
-	argv[tkns.size()] = NULL;
-
-	// Return argv
-	return argv;
-}
-
-
-// Free any memory that was allocated by conv_vec_to_chararray
-void dealloc_char_array(char** argv)
-{
-	// If no argument is specified
-	if (argv == NULL)
-	{
-		return;
-	}
-
-	for (int i = 0; argv[i] != NULL; ++i)
-	{
-		free(argv[i]);
-	}
-
-	// Free the array of any pointers
-	free(argv);
-}
-
-// Add an entry to the history list
-void hist_add(const string& cmd)
-{
-	// Exclude "r" and "exit" from the history
-	if (cmd == "r" || cmd.rfind("r ", 0) == 0 || cmd == "exit")
-	{
-		// Do nothing
-		return;
-	}
-
-	hist_ls.push_back(cmd);
-
-	if (hist_ls.size() > HIST_MAX)
-	{
-		hist_ls.erase(hist_ls.begin());
-	}
-}
-
-
-// Display history ("hist")
-void hist_display()
-{
-	// Print message
-	cout << "HISTORY:\n";
-
-	for (size_t i = 0; i < hist_ls.size(); ++i)
-	{
-		cout << " " << (i + 1) << ": " << hist_ls[i] << "\n";
-	}
-}
-
-
-// Logic for executing commands
-void exec_ext_cmd(const vector<string>& args)
-{
-	// If no arguments are specified
-	if (args.empty())
-	{
-		// Return
-		return;
-	}
-
-	// Convert to char**, in order for execvp to work
-	char** argv = conv_vec_to_chararray(args);
-	if (argv == NULL)
-	{
-		// Error message should be printed by the function, anyway.
-		return;
-	}
-
-	pid_t pid = fork();
-
-	// If the forking fails
-	if (pid == -1)
-	{
-		perror("[ERR] FORKING FAILED");
-		dealloc_char_array(argv);
-		return;
-	}
-	// Child process
-	else if (pid == 0)
-	{
-		// Execute specified command
-		execvp(argv[0], argv);
-
-		// If execvp returns anything, something isn't working. :(
-
-		cout << "[ERR] " << args[0] << " The program seems missing. Error code is: " << errno << endl;
-
-		// _exit() is supposed to be better. Why not?
-		// Exit with code 127, which corresponds to command not found
-		_exit(127);
-	}
-	// Parent process
-	else
-	{
-		int status;
-
-		// Wait for the child process to finish executing
-		pid_t term_pid = waitpid(pid, &status, 0);
-
-		// Bad :(
-		if (term_pid == -1)
-		{
-			perror("[ERR] WAITPID FAILED");
-		}
-		else
-		{
-			if (WIFEXITED(status))
-			{
-				int x_code = WEXITSTATUS(status);
-
-				// If there is a non-zero exit code
-				if (x_code != 0)
-				{
-					cout << "[ Program returned exit code " << x_code << "]\n";
-				}
-			}
-
-		else if (WIFSIGNALED(status))
-		{
-			cout << "[ Program terminated by signal " << WTERMSIG(status) << "]\n";
-		}
-
-	// Deallocate memory from argv
-	dealloc_char_array(argv);
-}
-
-
-int main(void)
-{
-	if (signal(SIGINT, sig_handler) == SIG_ERR)
-	{
-		perror("[ERR] COULD NOT REGISTER SIGINT HANDLER");
-		return EXIT_FAILURE;
-	}
-
-    while (1)
-    {
-	// Defined by limits.h, sets the maximum path length
-	char cwd[PATH_MAX];
-
-	if (getcwd(cwd, sizeof(cwd)) != nullptr)
-	{
-		cout << "GUISH (cmd #" << cmd_num << ") " << cwd " $ ";
-	}
-	else
-	{
-		perror("[ERR] FAILED GETCWD");
-		cout << "guish (cmd #" << cmd_num << ") $ ";
-	}
-
-	cout.flush();
-
-	string cmd_ln;
-
-	if (!getline(cin, cmd_ln))
-	{
-		// Handles EOF and input errors
-		if (cin.eof())
-		{
-			cout << "\n[INFO] SHELL EXITING.\n";
-		}
-		else
-		{
-			cerr << "[ERR] COULD NOT READ INPUT\n";
-		}
-
-		// Cleanup on exit, in the event of the input stream ending
-		cout << "Total SIGINT signals caught: " << sigint_counter << endl;
-		// No more shell
-		break;
-	}
-
-	// Ignore any empty input
-	if (cmd_ln.empty())
-	{
-		continue;
-	}
-
-	// Parse user input
-	vector<string> cmd_args;
-	parse_args(cmd_ln, cmd_args);
-
-	string cmd_name = cmd_args[0];
-
-        // prompt
-        cout << "SHELL$: ";
-
-        string cmd;
-        getline(cin, cmd);
-
-        // ignore empty input
-        if (cmd == "" || cmd.size() == 0) 
-            continue;
-
-        cout <<"Received user commands: ";
-        cout << cmd << endl;
-        
-        // built-in: exit
-        if (cmd == "help"){
-            cout << "GUISH - HELP MENU\n";
-		cout << "exit - Exits the shell.\n";
-		cout << "help - Displays this menu, explaining each command that is currently supported by guish.\n";
-		cout << "hist - Displays the 10 most recent commands. NOTE: The hist command will, itself, be added to the history.\n";
-		cout << "r <cmd> - Execute a command that is listed in the history. The command requires that the user specifies that number assigned to the command in the history.\n";
-            continue;
-        }else if(cmd == "exit"){
-		cout << "Total SIGINT signals caught: " << sig_counter << endl;
-            exit(0);
-        }
-	else if (cmd_name == "hist")
-	{
-		hist_add(cmd_ln);
-		hist_show();
-		cmd_num++;
-		continue;
-	}
-	else if (cmd_name == "r")
-	{
-		string hist_exec_cmd;
-		int hist_num = -1;
-
-		if (cmd_args.size() > 1)
-		{
-			try
-			{
-				hist_num = stoi(cmd_args[1]);
-
-        vector<string> cmd_args;
-        parse_args(cmd, cmd_args);
-
-        // fork child and execute program
-        int pid = fork();
-        int status;
-        if (pid == 0)
-        {
-            char * argv[4] = {"ls", "-l", "-a", NULL};
-            execvp(argv[0], argv);
-        }
-        else
-        {
-            // wait for program to finish and print exit status
-            waitpid(pid, &status, 0);
+    for (size_t i = 0; i < tkns.size(); ++i) {
+        argv[i] = strdup(tkns[i].c_str());
+        if (argv[i] == NULL) {
+            perror("\033[1;31m[ERR] STRDUP FAILED FOR ARGV\033[0m");
+            for (size_t j = 0; j < i; ++j) {
+                free(argv[j]);
+            }
+            free(argv);
+            return NULL;
         }
     }
+
+    argv[tkns.size()] = NULL;
+    return argv;
+}
+
+void dealloc_char_array(char** argv) {
+    if (argv == NULL) return;
+    for (int i = 0; argv[i] != NULL; ++i) {
+        free(argv[i]);
+    }
+    free(argv);
+}
+
+void hist_add(const string& cmd) {
+    if (cmd == "r" || cmd.rfind("r ", 0) == 0 || cmd == "exit") return;
+    history.push_back(cmd);
+    if (history.size() > HIST_MAX) {
+        history.erase(history.begin());
+    }
+}
+
+void hist_display() {
+    cout << "\033[0;37mHISTORY:\033[0m\n";
+    for (size_t i = 0; i < history.size(); ++i) {
+        cout << "\033[0;37m " << (i + 1) << ": " << history[i] << "\033[0m\n";
+    }
+}
+
+void exec_ext_cmd(const vector<string>& args) {
+    if (args.empty()) return;
+
+    char** argv = conv_vec_to_chararray(args);
+    if (argv == NULL) return;
+
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        perror("\033[1;31m[ERR] FORKING FAILED\033[0m");
+        dealloc_char_array(argv);
+        return;
+    } else if (pid == 0) {
+        execvp(argv[0], argv);
+        cout << "\033[1;31m[ERR] " << args[0] << " The program seems missing. Error code is: " << errno << "\033[0m" << endl;
+        _exit(127);
+    } else {
+        int status;
+        pid_t term_pid = waitpid(pid, &status, 0);
+
+        if (term_pid == -1) {
+            perror("\033[1;31m[ERR] WAITPID FAILED\033[0m");
+        } else {
+            if (WIFEXITED(status)) {
+                int x_code = WEXITSTATUS(status);
+                if (x_code != 0) {
+                    cout << "\033[0;37m[ Program returned exit code " << x_code << "]\033[0m\n";
+                }
+            } else if (WIFSIGNALED(status)) {
+                cout << "\033[0;37m[ Program terminated by signal " << WTERMSIG(status) << "]\033[0m\n";
+            }
+        }
+        dealloc_char_array(argv);
+    }
+}
+
+// A secret, cool function
+void covenant(const string& msg)
+{
+	const string colors[] =
+	{
+		// Red
+		"\033[1;31m",
+		// Yellow
+		"\033[1;33m",
+		// Green
+		"\033[1;32m",
+		// Cyan
+		"\033[1;36m",
+		// Blue
+		"\033[1;34m",
+		// Magenta
+		"\033[1;35m"
+		
+		// Any ideas as to what this does?
+	};
+	
+	size_t num_colors = sizeof(colors) / sizeof(colors[0]);
+	
+	for (size_t i = 0; i < msg.length(); ++i)
+	{
+		cout << colors[i % num_colors] << msg[i];
+	}
+
+	// Back to normal
+	cout << "\033[0m" << endl;
+}
+
+
+int main(void) {
+    if (signal(SIGINT, sig_handler) == SIG_ERR) {
+        perror("\033[1;31m[ERR] COULD NOT REGISTER SIGINT HANDLER\033[0m");
+        return EXIT_FAILURE;
+    }
+
+    while (1) {
+        char cwd[PATH_MAX];
+
+        if (getcwd(cwd, sizeof(cwd)) != nullptr) {
+            cout << "\033[1;32mGUISH (cmd #" << cmd_num << ") " << cwd << " $ \033[0m";
+        } else {
+            perror("\033[1;31m[ERR] FAILED GETCWD\033[0m");
+            cout << "\033[1;32mguish (cmd #" << cmd_num << ") $ \033[0m";
+        }
+
+        cout.flush();
+
+        string cmd_ln;
+        if (!getline(cin, cmd_ln)) {
+            if (cin.eof()) {
+                cout << "\033[1;34m\n[INFO] SHELL EXITING.\033[0m\n";
+            } else {
+                cerr << "\033[1;31m[ERR] COULD NOT READ INPUT\033[0m\n";
+            }
+
+            cout << "\033[0;37mTotal SIGINT signals caught: " << sigint_counter << "\033[0m\n";
+            break;
+        }
+
+        if (cmd_ln.empty()) continue;
+
+        vector<string> cmd_args;
+        parse_args(cmd_ln, cmd_args);
+
+        string cmd_name = cmd_args[0];
+
+        if (cmd_name == "help") {
+            cout << "\033[0;37mGUISH - HELP MENU\033[0m\n";
+            cout << "\033[0;37mexit - Exits the shell.\033[0m\n";
+            cout << "\033[0;37mhelp - Displays this menu, explaining each command that is currently supported by guish.\033[0m\n";
+            cout << "\033[0;37mhist - Displays the 10 most recent commands. NOTE: The hist command will, itself, be added to the history.\033[0m\n";
+            cout << "\033[0;37mr <cmd> - Execute a command that is listed in the history. The command requires that the user specifies that number assigned to the command in the history.\033[0m\n";
+            continue;
+        } else if (cmd_name == "exit") {
+            cout << "\033[0;37mTotal SIGINT signals caught: " << sigint_counter << "\033[0m\n";
+            exit(0);
+        } else if (cmd_name == "hist") {
+            hist_add(cmd_ln);
+            hist_display();
+            cmd_num++;
+            continue;
+		}
+		else if (cmd_name == "noah")
+		{
+			hist_add(cmd_ln);
+			covenant("NEVER AGAIN! :D");
+			cmd_num++;
+			continue;
+        } else if (cmd_name == "r") {
+            string hist_exec_cmd;
+            int hist_num = -1;
+
+            if (cmd_args.size() > 1) {
+                try {
+                    hist_num = stoi(cmd_args[1]);
+                } catch (const invalid_argument&) {
+                    cout << "\033[1;31m[ERR] INVALID NUMBER FORMAT.\nCorrect usage: r <number>\033[0m\n";
+                    cmd_num++;
+                    continue;
+                } catch (const out_of_range&) {
+                    cout << "\033[1;31m[ERR] NUMBER OUT OF RANGE.\nCorrect usage: r <number>\033[0m\n";
+                    cmd_num++;
+                    continue;
+                }
+            }
+
+            if (hist_num == -1) {
+                if (history.empty()) {
+                    cout << "\033[1;31m[ERR] NO HISTORY AVAILABLE.\033[0m\n";
+                    cmd_num++;
+                    continue;
+                }
+                hist_exec_cmd = history.back();
+            } else {
+                if (hist_num < 1 || hist_num > static_cast<int>(history.size())) {
+                    cout << "\033[1;31m[ERR] HISTORY NUMBER " << hist_num << " OUT OF RANGE\nCorrect usage: r <number>\033[0m\n";
+                    cmd_num++;
+                    continue;
+                }
+                hist_exec_cmd = history[hist_num - 1];
+            }
+
+            cout << "\033[0;37mRe-executing: " << hist_exec_cmd << "\033[0m\n";
+            hist_add(hist_exec_cmd);
+            vector<string> hist_cmd_args;
+            parse_args(hist_exec_cmd, hist_cmd_args);
+
+            if (!hist_cmd_args.empty()) {
+                if (hist_cmd_args[0] == "exit") {
+                    cout << "\033[0;37mTotal SIGINT signals caught: " << sigint_counter << "\033[0m\n";
+                    exit(0);
+                } else if (hist_cmd_args[0] == "hist") {
+                    hist_display();
+                } else {
+                    exec_ext_cmd(hist_cmd_args);
+                }
+            }
+
+            cmd_num++;
+            continue;
+        }
+
+        hist_add(cmd_ln);
+        exec_ext_cmd(cmd_args);
+        cmd_num++;
+    }
+
+    return 0;
 }
